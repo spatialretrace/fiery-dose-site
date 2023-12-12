0773a112a29a36dd9a57dc3da99fb138d00910ce
Firdous Nizar <48064033+spatialretrace@users.noreply.github.com>
2023-12-11 19:23:35 -0500

    Adding shop link

---
+++ Scripts/banner.js
@@ -0,0 +1,6 @@
+function insertHeader() {
+
+    var fdHeader = document.getElementById("fd-header");
+    var headerHTML = "<a href=\"index.html\"><img id=\"fd-logo\" src=\"Images/FD-Logo.png\"></img></a><h4>by Firdous</h4><img id=\"fd-banner\" src=\"Images/FD-PreviewImage.png\"></img>";
+    fdHeader.innerHTML = headerHTML;
+}
